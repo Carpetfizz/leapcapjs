ed9e42f5ffcf665d4ed2d83f0103ae40337aeed5
Ajay <aramesh.sj@mysummitps.org>
2014-02-28 12:30:14 -0800

    Record function returns bool, added license

--- leapcap.js
+++ leapcap.js
@@ -1,3 +1,31 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Ajay Ramesh
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * ---------------------------------------------------------------------------------
+ * Usage and examples can be found at https://github.com/Carpetfizz/leapcapjs
+ * To contact me you may use the following emails, or simply report a bug in GitHub
+ * carpetfizz@gmail.com
+ * aramesh.sj@mysummitps.org
+ */
+
 (function(LeapCap){
 /* THREE JS GLOBAL VARS*/
 var SCENE = new THREE.Scene();
@@ -173,10 +201,10 @@ LeapCap.play = function() {
 LeapCap.record = function(){
     RECORD_FRAMES = !RECORD_FRAMES;
     if(RECORD_FRAMES){
-        console.log("Started recording!");
+        return true;
     }
     else{
-        console.log("Stopped recording!");
+        return false;
     }
 }
 
